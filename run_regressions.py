c0323da8e3c5d3d3aa51934b81c95a162185d952
Marshall Ward <marshall.ward@gmail.com>
2019-04-19 00:27:27 -0400

    compiler sorting; general cleanup
    Since different compilers will reuse the same input path, and the
    clobber the output, we sort tests and regressions by compiler at the top
    level.  This way, we can batch our tests by compiler rather than
    configuration.
    
    Other minor overall cleanups as well, such as removal of fixed lists and
    generalisation of path namechanges for regression and test directories.

--- run_regressions.py
+++ run_regressions.py
@@ -4,52 +4,12 @@ import errno
 import os
 import shlex
 import subprocess
+import sys  # Testing
 
-import sys
-verbose = True
 
 DOC_LAYOUT = 'MOM_parameter_doc.layout'
+verbose = False
 
-
-compilers = [
-    'gnu',
-    'intel',
-    'pgi',
-]
-
-
-# TODO: Generate from regressions?
-model_drivers = [
-    'ocean_only',
-    'ice_ocean_SIS',
-    'ice_ocean_SIS2',
-    'coupled_AM2_LM3_SIS',
-    'coupled_AM2_LM3_SIS2',
-    'land_ice_ocean_LM3_SIS2',
-]
-
-
-def get_regression_tests(reg_path):
-    regression_tests = collections.defaultdict(list)
-
-    model_configs = os.listdir(reg_path)
-    for config in model_configs:
-        config_path = os.path.join(reg_path, config)
-        for path, _, files in os.walk(config_path):
-            # TODO: symmetric and static support
-            compilers = tuple(
-                os.path.splitext(f)[1].lstrip('.')
-                for f in files if f.startswith('ocean.stats')
-            )
-            if compilers:
-                # Replace 'regressions' with 'MOM6-examples'
-                r_s = path.index('regressions')
-                r_e = r_s + len('regressions')
-                test_path = path[:r_s] + 'MOM6-examples' + path[r_e:]
-
-                regression_tests[config].append((path, test_path, compilers))
-
-    return regression_tests
 
 def regressions():
     base_path = os.getcwd()
@@ -58,34 +18,38 @@ def regressions():
 
     # Check output
     if (verbose):
-        for driver in regression_tests:
-            print('{}: ['.format(driver))
-            for path in regression_tests[driver]:
-                print("    '{}',".format(path))
+        for compiler in regression_tests:
+            print('{}: ['.format(compiler))
+            for config in regression_tests[compiler]:
+                print("    '{}:',".format(config))
+                for reg, test in regression_tests[compiler][config]:
+                    print('        {}'.format(reg))
+                    #print('        {}'.format(test))
             print(']')
 
-    n_tests = sum(len(t) for t in regression_tests.values())
+    n_tests = sum(len(t) for t in regression_tests['gnu'].values())
     print('Number of tests: {}'.format(n_tests))
 
     # Temporarily dump output to /dev/null
     f_null = open(os.devnull, 'w')
 
-    for reg_test in regression_tests:
-        for test_path, _, _ in regression_tests[driver]:
-            layout_path = os.path.join(test_path, DOC_LAYOUT)
-            params = parse_mom6_param(layout_path)
+    # Switching compilers will currently clobber the old result!
+    # Maybe just do one at a time?
+    for compiler in regression_tests:
+        for config in regression_tests[compiler]:
+            for reg_path, test_path in regression_tests[compiler][config]:
+                layout_path = os.path.join(test_path, DOC_LAYOUT)
+                params = parse_mom6_param(layout_path)
 
-            ni = int(params['NIPROC'])
-            nj = int(params['NJPROC'])
-            nprocs = ni * nj
+                ni = int(params['NIPROC'])
+                nj = int(params['NJPROC'])
+                nprocs = ni * nj
 
-            # Just testing... trying 32 jobs
-            # And don't run concurrently
-            # And don't bother iterating over compilers
-            for compiler in compilers:
                 # TODO: repro, [no]symmetric, etc
-                exe_path = os.path.join(base_path, 'build', compiler, driver,
+                exe_path = os.path.join(base_path, 'build', compiler, config,
                                         'repro', 'MOM6')
+
+                # For now just test 1-node jobs
                 if nprocs <= 32:
                     # FMS requires that RESTART be created
                     os.chdir(test_path)
@@ -101,16 +65,48 @@ def regressions():
                         flags=srun_flags,
                         exe=exe_path
                     )
+                    print(test_path)
+                    print(cmd)
 
                     #print('Running {}...'.format(os.path.basename(test_path)))
-                    proc = subprocess.Popen(
-                        shlex.split(cmd),
-                        stdout=f_null,
-                        stderr=f_null,
-                    )
-                    print(proc.pid)
+                    #proc = subprocess.Popen(
+                    #    shlex.split(cmd),
+                    #    stdout=f_null,
+                    #    stderr=f_null,
+                    #)
+                    #print(proc.pid)
 
     f_null.close()
+
+
+def get_regression_tests(reg_path, test_dirname='MOM6-examples'):
+    regression_tests = {}
+
+    model_configs = os.listdir(reg_path)
+    for config in model_configs:
+        config_path = os.path.join(reg_path, config)
+        for path, _, files in os.walk(config_path):
+            # TODO: symmetric and static support
+            compilers = tuple(
+                os.path.splitext(f)[1].lstrip('.')
+                for f in files if f.startswith('ocean.stats')
+            )
+            if compilers:
+                reg_dirname = os.path.basename(reg_path.rstrip(os.sep))
+                r_s = path.index(reg_dirname)
+                r_e = r_s + len(reg_dirname)
+                test_path = path[:r_s] + test_dirname + path[r_e:]
+
+                # Old format
+                #regression_tests[config].append((path, test_path, compilers))
+                for compiler in compilers:
+                    if not compiler in regression_tests:
+                        regression_tests[compiler] = collections.defaultdict(list)
+
+                    test_record = path, test_path
+                    regression_tests[compiler][config].append(test_record)
+
+    return regression_tests
 
 
 def parse_mom6_param(path):
